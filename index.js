3aea6df03acd1770ac57d3cc13b94bce25ba2594
crossjs <liwenfu@crossjs.com>
2017-02-19 18:07:16 +0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 export default function () {
-  process.env.NODE_ENV === 'production' || console.log('please use `import xxx from \'directives/xxx\'` directly')
+  process.env.NODE_ENV === 'production' || console.log('please use `import xyz from \'platojs/directives/xyz\'` directly')
 }
